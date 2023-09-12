1fd846678d945195fe2057957e5c1784bbaf4d5c
fabien-ricca <fabien.ricca@laplateforme.io>
2023-09-12 10:39:23 +0200

    Jour5_job01

---
+++ Jour5/Job01/ArrayListArgumentExercice.java
@@ -0,0 +1,8 @@
+public class ArrayListArgumentExercice {
+
+    public static void display(String[] args){
+        for(String word : args){
+            System.out.println(word);
+        }
+    }
+}
